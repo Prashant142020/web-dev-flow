f783c03e88fa63484bfc83f654aba58d89a2c2bf
Prashant142020 <prashant142020@gmail.com>
2024-04-21 05:01:54 +0530

    add root folder

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,8 @@
- 
-
 export default function Home() {
     return (
-         
         <main>
             <h1 className="h1-bold">Hi we are live</h1>
             <h2 className="h2-bold">Hi we are live</h2>
         </main>
     );
 }
- 
