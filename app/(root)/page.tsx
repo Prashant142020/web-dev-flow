0e8da7d96d1bd4939a46c3bbc9d0d776225c85a9
Prashant142020 <prashant142020@gmail.com>
2024-04-21 15:36:01 +0530

    Add Shadcn UI , components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
+import Test from "@/components/test";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
         <main>
             <h1 className="h1-bold">Hi we are live</h1>
             <h2 className="h2-bold">Hi we are live</h2>
+            <Test />
+            <Button variant={"ghost"}>Click me</Button>
+            
         </main>
     );
 }
