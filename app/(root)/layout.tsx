9a66921f7c3dac028254a59d3895476a7e452d48
Prashant142020 <prashant142020@gmail.com>
2024-04-24 18:57:46 +0530

    add LeftSidebar upade MObileNav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import LeftSidebar from '@/components/shared/LeftSidebar';
 import NavBar from '@/components/shared/navbar/NavBar';
+import RightSidebar from '@/components/shared/RightSidebar';
 import { Toaster } from '@/components/ui/toaster';
 import React from 'react';
 
@@ -7,9 +9,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <NavBar />
       <div className="flex">
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+        <RightSidebar />
       </div>
 
       <Toaster />
