1453958a50c508171c9bb45bb7704a4dc96773fb
Prashant142020 <prashant142020@gmail.com>
2024-04-28 00:15:26 +0530

    rename : prisma to db

--- auth.ts
+++ auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth';
 import github from 'next-auth/providers/github';
 import google from 'next-auth/providers/google';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import prisma from './lib/prisma';
+import {PrismaAdapter  } from '@auth/prisma-adapter';
+import db from './lib/db';
 import { Adapter } from 'next-auth/adapters';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -10,6 +10,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
     logo: '/assets/images/logo.png',
   },
-  adapter: PrismaAdapter(prisma) as Adapter,
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [google, github],
 });
